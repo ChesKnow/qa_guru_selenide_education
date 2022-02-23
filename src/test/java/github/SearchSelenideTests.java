d2e290a06999893e4884aa865d475ebf79934085
Roman Chesanov <roman.chesanov>
2022-02-23 23:04:54 +0300

    build.gradle created

---
+++ src/test/java/github/SearchSelenideTests.java
@@ -0,0 +1,17 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchSelenideTests {
+
+    @Test
+    void shouldFindSelenideOnGithub() {
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$(".repo-list li").first().$("a").click();
+        $("h1").shouldHave(text("selenide / selenide"));
+    }
+}
